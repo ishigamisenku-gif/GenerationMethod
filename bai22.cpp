761df8a1d7843837394c906b3461555f7ed7d5f1
ishigamisenku-gif <anhkhuehuynh77@gmail.com>
2026-08-11 15:17:29 +0700

    Create bai22.cpp

---
+++ bai22.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define problem ""
+
+
+int main()
+{
+if (fopen(problem".INP","r")){
+    freopen(problem".INP","r",stdin);
+    freopen(problem".OUT","w",stdout);
+}
+    ll n;
+    cin>>n;
+    vector<ll>a(n,0);
+    while (true){
+        if (a[0]==1&&a[1]==1){
+        for (int i=0;i<n;i++){
+            cout<<a[i];
+        }
+        cout<<"\n";
+
+        }
+        ll i=n-1;
+        while (i>=0&&a[i]==1){
+            a[i]=0;
+            i--;
+        }
+        if (i<0)break;
+        a[i]=1;
+    }
+    return 0;
+}
